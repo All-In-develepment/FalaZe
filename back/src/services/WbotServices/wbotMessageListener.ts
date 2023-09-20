d3f370ecda257bce1a196388f9bb3022b14af14c
Gabriel Silva <biel.silva2802@gmail.com>
2023-09-20 10:18:58 -0300

    fix: corrigido as filas nos grupos

--- back/src/services/WbotServices/wbotMessageListener.ts
+++ back/src/services/WbotServices/wbotMessageListener.ts
@@ -55,7 +55,6 @@ import { ShowBaileysChatService } from "../BaileysChatServices/ShowBaileysChatSe
 import Whatsapp from "../../models/Whatsapp";
 import { FindWhoReceive } from "../../helpers/FindWhoReceive";
 
-
 // import { TelegramService } from "../TelegramServices/CreateTelegramService";
 
 const puppeteer = require("puppeteer");
@@ -4467,7 +4466,7 @@ const handleMessage = async (
 
     if (
       !ticket.queue &&
-      !isGroup &&
+      // !isGroup &&
       !msg.key.fromMe &&
       !ticket.userId &&
       whatsapp.queues.length >= 1
